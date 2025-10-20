57666f020a421880f04386b3484c09b57766a3ef
Kinuth <kinuthiaryan01@gmail.com>
2025-10-20 09:18:50 +0300

    add script to Mysql Server

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 -- This script inserts a single row into the Customers table
 -- in the alx_book_store database.
 USE alx_book_store;
-INSERT INTO Customer (customer_id, customer_name, email, address)
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
