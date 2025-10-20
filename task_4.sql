b618f2c152ae4602addd794fd64215e5cc71ef7f
Kinuth <kinuthiaryan01@gmail.com>
2025-10-20 09:02:51 +0300

    add script to Mysql Server

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- This script prints the full description of the 'Books' table
+-- from the database provided as an argument to the mysql command.
+-- It avoids using DESCRIBE or EXPLAIN.
+USE alx_book_store;
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'Books';
+
