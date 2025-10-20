5ef7399e1fd76cab91b3fb2715e2b5ca5cd183ea
Kinuth <kinuthiaryan01@gmail.com>
2025-10-20 09:48:21 +0300

    add script to Mysql Server

--- task_5.sql
+++ task_5.sql
@@ -2,4 +2,4 @@
 -- in the alx_book_store database.
 USE alx_book_store;
 INSERT INTO Customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness  Ave.');
